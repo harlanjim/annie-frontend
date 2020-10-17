e924431462e4ff28b76ae3da58c878efbb85de8c
fah.jim@gmail.com <fah.jim@gmail.com>
2020-10-17 22:14:47 +0000

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,43 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-app-bar app>
+      <h1>Annie</h1>
+      <!-- -->
+      <v-spacer />
+      <v-btn flat v-if="!$store.state.token" to="/Register">Register</v-btn>
+      <v-btn flat v-if="!$store.state.token" to="/Login">Login</v-btn>
+      <v-btn flat v-if="$store.state.token" @click="$store.commit('logout')"
+        >Logout</v-btn
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
+    <v-navigation-drawer>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <HelloWorld/>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router 
+        <router-view></router-view>-->
+      </v-container>
     </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    //    HelloWorld,
   },
 
   data: () => ({
